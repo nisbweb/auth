8bb6e0a1f1ba1608088bfa72a1645fbd9b33cacd
mridulganga <mridul.kepler@gmail.com>
2019-09-02 17:04:32 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify
+from db import *
+
+app = Flask(__name__)
+
+# Does nothing
+@app.route('/')
+def index():
+    return jsonify({
+        "status":"ok"
+    })
+
+@app.route("/auth", methods=["GET","POST","DELETE"])
+def auth_controller():
+    if request.method=="GET":
+        auths = get_auth(request.args.get("email"))
+        return jsonify(auths)
+
+    elif request.method=="POST":
+        req = request.get_json()
+        if verify_credential(req["email"],req["password"]):
+            token = add_auth(["email"])
+            return jsonify({"status":"ok","auth":token})
+        else:
+            return jsonify({"error":"wrong credentials","status":"error"})
+    
+    
+    elif request.method=="DELETE":
+        if request.args.get("all",False) == True:
+            delete_all_auth(request.args.get("email"))
+        else:
+            delete_auth(request.args.get("auth"))
+
+
+@app.route("/credential", methods=["GET","PUT","POST"])
+def credential_controller():
+    if request.method=="GET": # verify
+        pass
+
+    elif request.method=="PUT":
+        email = request.args.get("email")
+        password = request.args.get("password")
+        update_credential(email,password)
+        return jsonify({"status":"ok"})
+
+    elif request.method=="POST":
+        email = request.args.get("email")
+        password = request.args.get("password")
+        update_credential(email,password)
+        return jsonify({"status":"ok"})
+          
+
+if __name__ == '__main__':
+    # session["login"] = False
+
+    app.run(host='0.0.0.0', debug=True)
---
+++ db.py
@@ -0,0 +1,52 @@
+import pymongo
+import os
+import uuid
+
+client = pymongo.MongoClient(os.environ["MONGO"])
+db = client.auth
+
+def verify_credential(email,password):
+    ms = db.credentials.find_one({"email":email,"password":password})
+    if ms:
+        return True
+    return False
+
+def add_credential(email,password):
+    credential = {
+        "email":email,
+        "password":password
+    }
+    db.credentials.insert_one(credential)
+
+def update_credential(email,password):
+    credential = {
+        "email":email,
+        "password":password
+    }
+    db.credentials.find_one_and_replace({"email":email},credential)
+
+
+
+def add_auth(email):
+    token = uuid.uuid4().hex
+    db.auths.insert_one({
+        "email":email,
+        "token":token
+    })
+    return token
+
+def get_auth(email):
+    auths_list=[]
+    auths = db.auths.find({"email":email})
+    if auths:
+        for a in auths:
+            auths_list.append(a["token"])
+    return auths_list
+
+
+def delete_all_auth(email):
+    db.auths.delete({"email":email})
+
+def delete_auth(token):
+    db.auths.delete_one({"token":token})
+
