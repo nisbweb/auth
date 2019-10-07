4faaf852c50d9b780f70b0393691f3e76d3fd7b7
mridulganga <mridul-ganga@hpe.com>
2019-10-07 20:24:56 +0530

    using yandex mail

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import yagmail
+import smtplib
 import os
 from random import randint
 
@@ -8,14 +8,18 @@ def generate_token(digits):
     return str(randint(range_start, range_end))
 
 def send_email_with_token(email, token):
-    yag = yagmail.SMTP(os.environ["EMAIL_USERNAME"], os.environ["EMAIL_PASSWORD"])
-    print(os.environ["EMAIL_USERNAME"], os.environ["EMAIL_PASSWORD"])
-    contents = ['An attempt was made to reset the password.',
-            'Please use the following as token for the same', token, 
-            ' If you did not try to reset the password, you don\'t need to do anything.']
-    yag.send(email, 'NISB Password Reset', contents)
-
+    server = smtplib.SMTP_SSL('smtp.yandex.com:465')
+    server.ehlo()
+    server.login(os.environ["EMAIL_USERNAME"],os.environ["EMAIL_PASSWORD"])
+    message = 'Subject: NISB Password Reset\n\n' + \
+            'An attempt was made to reset the password.\n' + \
+            'Please use the following as token for the same\n' + token + \
+            '\nIf you did not try to reset the password, you don\'t need to do anything.'
+    server.sendmail(os.environ["EMAIL_USERNAME"],email,message)
+    server.quit()
+    return True
 
 
 # if __name__ == "__main__":
-#     print(generate_token(6))
+#     token = (generate_token(6))
+#     send_email_with_token("mridul.kepler@gmail.com",token)
